e507f51c0a959e8adf8db8915023b871e7015662
BadMannersBM <bruno.fdm93@gmail.com>
2020-08-06 15:05:13 -0700

    The Reducer Function

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+
+function todos (state =[], action){
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
 function createStore () {
-  // The store shoould have four parts
+  // The store should have four parts
   // 1. The state
   // 2. Get the state.
   // 3. Listen to changes on the state.
   // 4. Update the state
 
   let state
+  let listeners = []
 
   const getState = () => state
 
-  return {
-    getState
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
   }
 
+  return {
+    getState,
+    subscribe
+  }
 }
