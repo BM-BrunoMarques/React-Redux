25c641e1b9ea9f1f4a368363fe4e4d6895984518
BadMannersBM <bruno.fdm93@gmail.com>
2020-08-16 18:51:02 -0700

    More Actions

--- index.js
+++ index.js
@@ -32,9 +32,36 @@ function createStore (reducer) {
 
 //App Code
 function todos (state =[], action){
+  switch (action.type){
+    case 'ADD_TODO' :
+      return state.concat([action.todo])
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO' :
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.assign({}, todo, { complete: !todo.complete }))
+    default :
+      return state
+  }
+}
+
   if (action.type === 'ADD_TODO') {
     return state.concat([action.todo])
   }
 
   return state
 }
+
+const store = createStore(todos)
+store.subscribe(() => {
+  console.log('The new state is: ' , store.getState())
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo:{
+    id: 0,
+    name: 'Learn Readux',
+    complete: false
+  }
+})
